4317bc914f8176cc46f0e76754adb11e7ec5cc32
uncletoon <ingabirepatience3@gmail.com>
2026-06-12 12:17:29 +0200

    refactor: organize frontend into client/ and dashboard/ directories

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,23 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, Outlet } from "react-router-dom";
 import { useEffect } from "react";
-import { Navbar, Footer } from "./components/Layout";
-import ScrollToTop from "./components/ScrollToTop";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About";
-import { Medicines } from "./pages/Medicines";
-import { Services } from "./pages/Services";
-import { Facilities } from "./pages/Facilities";
-import { FacilityDetail } from "./pages/FacilityDetail";
-import ServiceDetail from "./pages/ServiceDetails";
+import { Navbar, Footer } from "./client/components/Layout";
+import ScrollToTop from "./client/components/ScrollToTop";
+import { Home } from "./client/pages/Home";
+import { About } from "./client/pages/About";
+import { Medicines } from "./client/pages/Medicines";
+import { Services } from "./client/pages/Services";
+import { Facilities } from "./client/pages/Facilities";
+import { FacilityDetail } from "./client/pages/FacilityDetail";
+import ServiceDetail from "./client/pages/ServiceDetails";
+
+// Import Admin Dashboard components and pages
+import AppLayout from "./dashboard/layout/AppLayout";
+import DashboardHome from "./dashboard/pages/Dashboard/Home";
+import AdminServices from "./dashboard/pages/Services";
+import AdminMedicines from "./dashboard/pages/Medicines";
+import Bookings from "./dashboard/pages/Bookings";
+import Analytics from "./dashboard/pages/Analytics";
+import UserProfile from "./dashboard/pages/UserProfile";
 
 function RouteScroller() {
   const { pathname } = useLocation();
@@ -18,30 +27,50 @@ function RouteScroller() {
   return null;
 }
 
+function PublicLayout() {
+  return (
+    <div className="flex flex-col min-h-screen bg-surface">
+      <Navbar />
+      <main className="grow">
+        <Outlet />
+      </main>
+      <Footer />
+      <ScrollToTop />
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <RouteScroller />
         <ScrollToTop />
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/medicines" element={<Medicines />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/services/:id" element={<ServiceDetail />} />
-              <Route path="/facilities" element={<Facilities />} />
-              <Route path="/facilities/:id" element={<FacilityDetail />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/emergency" element={<Services />} />
-            </Routes>
-          </main>
-          <Footer />
-          <ScrollToTop />
-        </div>
+        <Routes>
+          {/* Admin Dashboard Routes */}
+          <Route path="/admin" element={<AppLayout />}>
+            <Route index element={<DashboardHome />} />
+            <Route path="services" element={<AdminServices />} />
+            <Route path="medicines" element={<AdminMedicines />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="analytics" element={<Analytics />} />
+            <Route path="profile" element={<UserProfile />} />
+          </Route>
+
+          {/* Public Website Routes */}
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/medicines" element={<Medicines />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/services/:id" element={<ServiceDetail />} />
+            <Route path="/facilities" element={<Facilities />} />
+            <Route path="/facilities/:id" element={<FacilityDetail />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/emergency" element={<Services />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
 }
+
